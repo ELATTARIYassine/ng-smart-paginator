ca898f03724670a9a4d9752a066fdd190f926fed
El attari Yassine <yassineattari64@gmail.com>
2020-01-30 13:55:47 +0100

    [homeC] set the unsubscribe logic

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse, HttpParams } from "@angular/common/http";
+import { throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +14,21 @@ export class DataService {
   constructor(private httpClient: HttpClient) { }
 
   public sendGetRequest(){
-    return this.httpClient.get(this.REST_API_SERVER);
+    // Add safe, URL encoded_page parameter 
+    const options = { params: new HttpParams({fromString: "_page=1&_limit=20"}) };
+    return this.httpClient.get(this.REST_API_SERVER, options).pipe(retry(3), catchError(this.handleError));
+  }
+  
+  handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Client-side errors
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Server-side errors
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    window.alert(errorMessage);
+    return throwError(errorMessage);
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataService } from '../data.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy  {
 
   products = [];
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.sendGetRequest().subscribe((data: any[])=>{
+    this.dataService.sendGetRequest().pipe(takeUntil(this.destroy$)).subscribe((data: any[])=>{
       console.log(data);
       this.products = data;
     })  
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Unsubscribe from the subject
+    this.destroy$.unsubscribe();
   }
 
 }
